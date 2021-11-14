47d1bd86c51f0ed00a75bff3b6a549ece91be3de
nanikon <nata.nik.18@mail.ru>
2021-11-14 20:42:58 +0300

    add entity

---
+++ src/main/java/com/example/third/model/ShotEntity.java
@@ -0,0 +1,37 @@
+package com.example.third.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ * @author Natalia Nikonova
+ */
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "shots")
+public class ShotEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+    @Column(name = "x")
+    private double x;
+    @Column(name = "y")
+    private double y;
+    @Column(name = "r")
+    private int r;
+    @Column(name = "is_hit")
+    private boolean isHit;
+}
---
+++ src/main/java/com/example/third/model/UserEntity.java
@@ -0,0 +1,26 @@
+package com.example.third.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author Natalia Nikonova
+ */
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "session_id")
+    private String sessionId;
+}
