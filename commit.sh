0fd90b55ecff2b9d94103ee661dc5f0ed5979caf
Mariya Senina <senina.m.m@gmail.com>
2022-04-21 18:45:49 +0300

    naming in commit.sh changed to more pretty one + task name changed to diff

--- commit.sh
+++ commit.sh
@@ -1,18 +1,15 @@
-#!/bin/bash
-echo "Commit checker started"
 CLASSES="${PWD}/commit_classes"
-
+echo $CLASSES  
 ANY_CHANGES=false
 
 while CLASSES= read -r LINE
 do
     SVN_STATUS=`svn status ${PWD}/${LINE}`
-    #echo "${LINE}"
     echo $SVN_STATUS
-    if  [[ SVN_STATUS -eq "" ]]; then
-          echo "\$SVN_STATUS is empty => file wasn't modified"
+    if  [[ -z $SVN_STATUS ]]; then
+          echo "${LINE} wasn't modified"
     else
-          echo "\$SVN_STATUS is NOT empty => file was modified and ready to commit"
+          echo "${LINE} was modified and ready to commit"
           ANY_CHANGES=true
     fi
 done < "${CLASSES}"
